f83dc49c411f487fd461f967dcf0c8cf79c6dfc2
pouya <pouya@radhoosh.ir>
2022-10-21 14:45:20 +0330

    Stake() Gas opt

--- contracts/staking.sol
+++ contracts/staking.sol
@@ -2,16 +2,24 @@
 pragma solidity 0.8.17;
 
     import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+    import "@openzeppelin/contracts/utils/Context.sol";
 
 
-contract staking{
+contract staking is Context{
     IERC20 public stakingToken;
-    
+    mapping(address => uint256) public balances;
+    uint256 public totalSupply;
+
     constructor(address tokenAddress){
         stakingToken = IERC20(tokenAddress);
     }
 
     function stake(uint256 tokenAmount) external {
-        
+       require(stakingToken.transferFrom(_msgSender(),address(this),tokenAmount));
+
+        balances[_msgSender()] += tokenAmount;
+        totalSupply += tokenAmount;
     }
+
+    
 }
